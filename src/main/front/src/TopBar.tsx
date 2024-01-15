9106189150d828dc59c30c752ee0e21ee0843941
Backjinuk <94981312+Backjinuk@users.noreply.github.com>
2024-01-16 00:02:21 +0900

    [BJW] 2024-01-15 TopBar에 임시 알림 보내기 추가

--- src/main/front/src/TopBar.tsx
+++ src/main/front/src/TopBar.tsx
@@ -26,27 +26,52 @@ export default function TopBar({ ChangeEvent }: any) {
     }
   }, []);
 
-  return (
-    <Box sx={{ flexGrow: 1, my: 2 }}>
-      <AppBar position='static'>
-        <Toolbar variant='dense' sx={{ height: '60px' }}>
-          <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <CreateLeger ChangeEvent={ChangeEvent} categoryList={categoryList} />
-          <Button sx={{ color: '#fff' }} variant='outlined' onClick={() => navigate('/MapLocation')}>
-            지도 모아보기
-          </Button>
-          <Button sx={{ color: '#fff' }} variant='outlined' onClick={() => navigate('/Ledger')}>
-            메인페이지
-          </Button>
-          <Button sx={{ color: '#fff' }} variant='outlined' onClick={() => navigate('/calender')}>
-            달력으로 보기
-          </Button>
-          <UserInfoButton />
-          <Logout />
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    const  notification = () => {
+
+        Notification.requestPermission().then(function(permission) {
+            if (permission === 'granted') {
+                console.log('Notification permission granted.');
+                // 알림을 보냅니다.
+                var notification = new Notification('Hello, User!', {
+                    body: '알림을 보냅니다.',
+                    icon: 'https://i.namu.wiki/i/Bbq0E9hXYyrXbL4TnIE__vtQ2QwiZ3i40NZSLiX_a6S0ftYCndVZjf4vlruWur4I3Z0o7CZuFrRMl2CKxyk30w.webp',
+                    vibrate: [100, 50, 100],
+                    tag: 'nasdak'
+                });
+
+                notification.onclick = () => {
+                    window.open('https://naver.com', '_blank');
+                    notification.close();
+                }
+
+            }
+        });
+
+    }
+
+
+    return (
+        <Box sx={{ flexGrow: 1, my: 2 }}>
+            <AppBar position="static">
+                <Toolbar variant="dense" sx={{ height: '60px' }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <CreateLeger ChangeEvent={ChangeEvent} categoryList={categoryList} />
+                    <Button sx={{ color: '#fff' }} variant="outlined" onClick={() => navigate("/MapLocation")}>지도 모아보기</Button>
+                    <Button sx={{ color: '#fff' }} variant="outlined" onClick={() => navigate("/Ledger")}>메인페이지</Button>
+                    <Button sx={{ color: '#fff' }} variant="outlined" onClick={() => navigate("/calender")}>달력으로 보기</Button>
+                    <Button sx={{ color: '#fff' }} variant="outlined" onClick={() =>  notification()}>알림 보내기</Button>
+                    <UserInfoButton />
+                    <Logout />
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }

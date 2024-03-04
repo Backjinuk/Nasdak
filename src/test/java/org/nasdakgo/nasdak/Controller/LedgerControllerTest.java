7add55b893361569dad4aada63d6539c5832dcba
BJW <backj123@naver.com>
2024-03-04 23:59:45 +0900

    [BJW]2024-03-03 LedgerList 조회 버그 수정중

--- src/test/java/org/nasdakgo/nasdak/Controller/LedgerControllerTest.java
+++ src/test/java/org/nasdakgo/nasdak/Controller/LedgerControllerTest.java
@@ -18,6 +18,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class LedgerControllerTest {
@@ -30,14 +32,17 @@ class LedgerControllerTest {
 
     private final ModelMapper modelMapper;
 
+    private final LedgerController ledgerController;
+
     @Value("${upload.file.path}")
     String filePath;
 
     @Autowired
-    LedgerControllerTest(LedgerService ledgerService, FilesService filesService, ModelMapper modelMapper) {
+    LedgerControllerTest(LedgerService ledgerService, FilesService filesService, ModelMapper modelMapper, LedgerController ledgerController) {
         this.ledgerService = ledgerService;
         this.filesService = filesService;
         this.modelMapper = modelMapper;
+        this.ledgerController = ledgerController;
     }
 
     @Test
@@ -177,7 +182,86 @@ class LedgerControllerTest {
 
 
     }
-    
-    
+
+    @Test
+    public void testLedgerAllList2(){
+        UserDto usersDto = new UserDto();
+        usersDto.setUserNo(1L);
+
+        LocalDate startDate = LocalDate.parse("2024-01-28");
+
+        LocalDate endDate = null;
+
+        String searchKey = "Week";
+        String prevNext = "first";
+
+
+        Map<String, Object> dateMap = ledgerController.searchDate(prevNext, searchKey, startDate, endDate);
+
+        AtomicReference<Ledger> previousLedger = new AtomicReference<>();
+
+        long userNo = usersDto.getUserNo();
+
+        List<LedgerDto> allByUsers2 = new ArrayList<>();
+
+
+        allByUsers2 = ledgerService.getLedgerList(LocalDate.parse((String) dateMap.get("startDate")), LocalDate.parse((String) dateMap.get("endDate")), userNo)
+                .stream()
+                .peek(previousLedger::set)
+                .map(ledger -> modelMapper.map(ledger, LedgerDto.class))  // Ledger를 LedgerDto로 변환
+                .toList();  // LedgerDtoList를 조회
+
+        Map<String, List<?>> stringListMap = ledgerController.TranformMap(allByUsers2, searchKey);
+
+        int breakCount = 0;
+
+        if(stringListMap.keySet().size() <= 3){
+            while (stringListMap.keySet().size() < 3) {
+                List<String> keys = new ArrayList<>(stringListMap.keySet());
+                String lastKey = keys.get(keys.size() - 1);
+                LocalDate keyDate = LocalDate.parse(lastKey.split("~")[0].trim());
+
+                LocalDateTime ledgerSearchDate = ledgerService.getLedgerSearchDate(keyDate, userNo); // 재조정된 날짜를 기반으로 가장 최근의 데이터가 있는 날짜를 조회
+
+                Map<String, Object> searchMap = ledgerController.searchDate("OneDay", searchKey, LocalDate.from(ledgerSearchDate), endDate);// 재조정된 날짜로 검색조건 재설정
+
+                List<LedgerDto> collect = ledgerService.getLedgerList(LocalDate.parse(String.valueOf(searchMap.get("startDate"))),
+                                LocalDate.parse(String.valueOf(searchMap.get("endDate"))),
+                                userNo)
+                        .stream()
+                        .map(ledger -> modelMapper.map(ledger, LedgerDto.class))
+                        .collect(Collectors.toList());
+
+                Map<String, List<?>> tempStringListMap = ledgerController.TranformMap(collect, searchKey);
+
+                System.out.println("tempStringListMap = " + tempStringListMap);
+
+
+
+                for (String key : tempStringListMap.keySet()) {
+                    if (!stringListMap.containsKey(key)) {
+                        stringListMap.putAll(tempStringListMap);
+                        break;
+                    }
+                }
+
+
+                breakCount++;
+
+                if (breakCount > 4) {
+                    System.out.println("데이터가 더이상 없다.");
+                    break;
+                }
+            }
+
+            for (String key : stringListMap.keySet()) {
+                System.out.println("Key: " + key);
+                System.out.println("Value: " + stringListMap.get(key));
+            }
+        }
+
+
+
+    }
     
 }

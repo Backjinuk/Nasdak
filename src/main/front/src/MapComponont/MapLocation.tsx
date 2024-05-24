4dd3659ba122718091b4c4a13cdf4d7989aae395
BJW <backj123@fgi.kr>
2024-05-24 18:20:02 +0900

    [BJW]2024/05/24 날짜별 지도보기 & 지도 모아보기 통합 개발중

--- src/main/front/src/MapComponont/MapLocation.tsx
+++ src/main/front/src/MapComponont/MapLocation.tsx
@@ -1,277 +1,253 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import './Map.css';
-import { CategoryType, LedgerType, location } from '../TypeList';
+import {CategoryType, LedgerType} from '../TypeList';
 import LedgerDetail from '../LedgerComponont/LedgerDetail';
 import Swal from 'sweetalert2';
-import { useAppDispatch, useAppSelector } from 'app/hooks';
-import { RootState } from '../app/store';
-import { axiosGetLedgerDetail } from '../app/slices/ledgerSilce';
+import {useAppDispatch, useAppSelector} from 'app/hooks';
+import {RootState} from '../app/store';
+import {axiosGetLedgerDetail} from '../app/slices/ledgerSilce';
 import axios from "axios";
-import {getJsonHeader} from "../headers";
 import {getCookie} from "../Cookies";
 
 // @ts-ignore
 const {kakao} = window;
 
-export default function MapLocation({ event, locationList }: {event : any, locationList : number[] }) {
-  const dispatch = useAppDispatch();
-  const ledgerSeqNumbers = useAppSelector((state: RootState) => state.ledger.ledgerSeqNumbers);
-  const ledger = useAppSelector((state: RootState) => state.ledger.ledger);
-  const [categoryList, setCategoryList] = useState<CategoryType[]>([]);
-  const [changeEvent, setChangeEvent] = useState(false);
-  const [open, setOpen] = useState<boolean>(false);
-  const selectButton = useAppSelector((state: RootState) => state.ledger.selectButton);
+export default function MapLocation({event, locationList}: { event: any, locationList: number[] }) {
+    const dispatch = useAppDispatch();
+    const ledgerSeqNumbers = useAppSelector((state: RootState) => state.ledger.ledgerSeqNumbers);
+    const ledger = useAppSelector((state: RootState) => state.ledger.ledger);
+    const [categoryList, setCategoryList] = useState<CategoryType[]>([]);
+    const [changeEvent, setChangeEvent] = useState(false);
+    const [open, setOpen] = useState<boolean>(false);
+    const selectButton = useAppSelector((state: RootState) => state.ledger.selectButton);
+    const [infowindow, setInfowindow] = useState<kakao.maps.InfoWindow | null>(null);
+    const [map, setMap] = useState<kakao.maps.Map | undefined>();
+    let markers: kakao.maps.Marker[] = [];
+
+    useEffect(() => {
+        const mapContainer = document.getElementById('MapLocation2'); // 지도를 표시할 div
+        const mapOption = {
+            center: new kakao.maps.LatLng(37.5665, 126.9780), // 지도의 중심 좌표 (서울)
+            level: 1, // 지도의 확대 레벨
+        };
+
+        if (kakao && kakao.maps && mapContainer) {
+            const newMap = new kakao.maps.Map(mapContainer, mapOption);
+            const newInfowindow = new kakao.maps.InfoWindow({zIndex: 1});
+
+            setMap(newMap);
+            setInfowindow(newInfowindow);
+        }
+    }, [])
+
+    useEffect(() => {
+    console.log(map)
+        const triggerClick = async () => {
+            if (map && infowindow) {
+                if (ledgerSeqNumbers === null) {
+                    try {
+                        const res = await axios.get('/api/ledger/locationList', {
+                            headers: {
+                                Authorization: `Bearer ${getCookie('accessToken')}`,
+                                'Content-Type': 'application/json',
+                            }
+                        });
+                        await displayPlaces(res.data);
+                    } catch (error) {
+                        console.error('Error fetching location list:', error);
+                    }
+                } else {
+                    setTimeout(async () => {
+                        await displayPlaces(ledgerSeqNumbers);
+                        $('.markerbg.marker_1').click();
+                    }, 500);
+                }
+            }
+        };
+
+        triggerClick();
+    }, [map, infowindow]);
 
 
+    function displayPlaces(places: LedgerType[]): Promise<void> {
+        return new Promise((resolve) => {
 
-  useEffect(() => {
-    var markers: any[] = [];
+            const listEl = document.getElementById('placesList');
+            const menuEl = document.getElementById('menu_wrap');
+            const fragment = document.createDocumentFragment();
+            const bounds = new kakao.maps.LatLngBounds();
 
-      axios.get('/api/ledger/locationList',  {
-        headers : {
-          Authorization: `Bearer ${getCookie('accessToken')}`,
-          'Content-Type': 'application/json',
-        }}
-      ).then((res) => {
-        displayPlaces(res.data);
+            removeAllChildNodes(listEl);
+            removeMarker();
 
-      });
+            places.forEach((place, i) => {
+                const placePosition = new kakao.maps.LatLng(place.location.y, place.location.x);
+                const marker = addMarker(placePosition, i, place.comment);
+                const itemEl = getListItem(i, place); // 검색 결과 항목 Element를 생성합니다
 
+                bounds.extend(placePosition);
 
+                kakao.maps.event.addListener(marker, 'mouseover', () => {
+                    displayInfowindow(marker, place.location.address);
+                });
 
-    var mapContainer = document.getElementById('MapLocation'), // 지도를 표시할 div
-      mapOption = {
-        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
-      };
+                kakao.maps.event.addListener(marker, 'click', () => {
+                    MapLedgerDetailFn(place.fileOwnerNo);
+                });
 
-    // 지도를 생성합니다
-    var map = new kakao.maps.Map(mapContainer as HTMLElement, mapOption);
+                kakao.maps.event.addListener(marker, 'mouseout', () => {
+                    if (infowindow) {
+                        infowindow.close();
+                    }
+                });
 
-    // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
-    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+                itemEl.onclick = () => {
+                    displayInfowindowMoveLocation(marker, place.location.address);
+                };
 
-    // 검색 결과 목록과 마커를 표출하는 함수입니다
-    function displayPlaces(places: LedgerType[]) {
-      var listEl = document.getElementById('placesList'),
-        menuEl = document.getElementById('menu_wrap'),
-        fragment = document.createDocumentFragment(),
-        bounds = new kakao.maps.LatLngBounds(),
-        listStr = '';
+                itemEl.onmouseout = () => {
+                    if (infowindow) {
+                        infowindow.close();
+                    }
+                };
 
+                fragment.appendChild(itemEl);
+            });
 
-      // 검색 결과 목록에 추가된 항목들을 제거합니다
-      removeAllChildNods(listEl);
+            if (listEl && menuEl) {
+                listEl.appendChild(fragment);
+                menuEl.scrollTop = 0;
+            }
 
-      // 지도에 표시되고 있는 마커를 제거합니다
-      removeMarker();
+            if (map) {
+                map.setBounds(bounds);
+            }
 
-      for (var i = 0; i < places.length; i++) {
-        // 마커를 생성하고 지도에 표시합니다
-
-        var placePosition = new kakao.maps.LatLng(places[i].location.y, places[i].location.x),
-          // @ts-ignore
-          marker = addMarker(placePosition, i, places[i].comment),
-          itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
-
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해z
-        // LatLngBounds 객체에 좌표를 추가합니다
-        bounds.extend(placePosition);
-
-        // 마커와 검색결과 항목에 mouseover 했을때
-        // 해당 장소에 인포윈도우에 장소명을 표시합니다
-        // mouseout 했을 때는 인포윈도우를 닫습니다
-        // @ts-ignore
-        (function (marker, title, fileOwnerNo) {
-          kakao.maps.event.addListener(marker, 'mouseover', function () {
-            displayInfowindow(marker, title);
-          });
-
-          kakao.maps.event.addListener(marker, 'click', function () {
-            MapLedgerDetailFn(fileOwnerNo);
-            //alert(fileOwnerNo);
-          });
-
-          kakao.maps.event.addListener(marker, 'mouseout', function () {
-            infowindow.close();
-          });
-
-          /*                    itemEl.onmouseover =  function () {
-                        displayInfowindow(marker, title);
-                    };
-*/
-
-          itemEl.onclick = function () {
-            displayInfowindowMoveLocation(marker, title);
-          };
-
-          itemEl.onmouseout = function () {
-            infowindow.close();
-          };
-        })(marker, places[i].location.address, places[i].fileOwnerNo);
-
-        fragment.appendChild(itemEl);
-      }
-
-      // 검색결과 항목들을 검색결과 목록 Element에 추가합니다
-      // @ts-ignore
-      listEl.appendChild(fragment);
-      // @ts-ignore
-      menuEl.scrollTop = 0;
-
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-
-      map.setBounds(bounds);
+            resolve(); // 프라미스를 완료합니다.
+        })
     }
 
     // 검색결과 항목을 Element로 반환하는 함수입니다
-    function getListItem(index: number, places: LedgerType) {
-      var el = document.createElement('li'),
-        itemStr =
-          '<span class="markerbg marker_' +
-          (index + 1) +
-          '"></span>' +
-          '<div style="display : flex;" >' +
-          '   <div class="info">' +
-          '       <h5>' +
-          places.comment +
-          '</h5>' +
-          '       <span>' +
-          places.location.address +
-          '</span>' +
-          '       <span class="tel">' +
-          places.price +
-          '</span>' +
-          '   </div>' +
-          '   <div>' +
-          '      <button class="MapLedgerUpdate" value="' +
-          places.fileOwnerNo +
-          '">수정</button>' +
-          '      <button class="MapLedgerDelete" value="' +
-          places.fileOwnerNo +
-          '">삭제</button>' +
-          '   </div>' +
-          '</div>';
-
-      el.innerHTML = itemStr;
-      el.className = 'item';
-
-      return el;
+    function getListItem(index: number, place: LedgerType) {
+        const el = document.createElement('li');
+        const itemStr = `
+    <span class="markerbg marker_${index + 1}"></span>
+    <div style="display: flex;">
+    <div class="info">
+        <h5>${place.comment}</h5>
+        <span>${place.location.address}</span>
+        <span class="tel">${place.price}</span>
+    </div>
+    <div>
+        <button class="MapLedgerUpdate" value="${place.fileOwnerNo}">수정</button>
+        <button class="MapLedgerDelete" value="${place.fileOwnerNo}">삭제</button>
+    </div>
+    </div>
+    `;
+        el.innerHTML = itemStr;
+        el.className = 'item';
+        return el;
     }
 
-    // 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
-    function addMarker(position: any, idx: number, title: undefined) {
-      //let placePosition = new kakao.maps.LatLng(y, x)
+    function addMarker(position: kakao.maps.LatLng, idx: number, title: string) {
+        const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png'; // 마커 이미지 url
+        const imageSize = new kakao.maps.Size(36, 37); // 마커 이미지의 크기
+        const imgOptions = {
+            spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+            spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+            offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+        };
+        const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions);
 
-      var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
-        imageSize = new kakao.maps.Size(36, 37), // 마커 이미지의 크기
-        imgOptions = {
-          spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-          spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-          offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-        },
-        markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions);
-      var marker = new kakao.maps.Marker({
-        position: position,
-        image: markerImage,
-        map: map,
-      });
+        const marker = new kakao.maps.Marker({
+            position,
+            image: markerImage,
+            map: map,
+        });
 
-      /*
-            marker.setImage(markerImage)
-            marker.setPosition( position )
-            marker.setMap(map);
-*/
-      // 지도 위에 마커를 표출합니다
-      markers.push(marker); // 배열에 생성된 마커를 추가합니다
-
-      return marker;
-    }
-
-    // 지도 위에 표시되고 있는 마커를 모두 제거합니다
-    function removeMarker() {
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-      }
-      markers = [];
+        markers.push(marker); // 배열에 생성된 마커를 추가합니다
+        return marker;
     }
 
     // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
     // 인포윈도우에 장소명을 표시합니다
-    function displayInfowindow(marker: any, title: string) {
-      var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
-
-      infowindow.setContent(content);
-      infowindow.open(map, marker);
+    function displayInfowindow(marker: kakao.maps.Marker, title: string) {
+        const content = `<div style="padding:5px;z-index:1;">${title}</div>`;
+        if (infowindow && map) {
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+        }
     }
 
-    function displayInfowindowMoveLocation(marker: any, title: string) {
-    console.log(marker)
-      alert(title)
-      var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
+    function displayInfowindowMoveLocation(marker: kakao.maps.Marker, title: string) {
+        const content = `<div style="padding:5px;z-index:1;">${title}</div>`;
 
-      infowindow.setContent(content);
-      infowindow.open(map, marker);
-
-      map.setCenter(marker.getPosition());
-      map.setLevel(4);
+        if (infowindow && map) {
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+            map.setCenter(marker.getPosition());
+            map.setLevel(4);
+        }
     }
 
-    // 검색결과 목록의 자식 Element를 제거하는 함수입니다
-    function removeAllChildNods(el: HTMLElement | null) {
-      // @ts-ignore
-      while (el.hasChildNodes()) {
-        // @ts-ignore
-        el.removeChild(el.lastChild);
-      }
+    function removeAllChildNodes(el: HTMLElement | null) {
+        if (el) {
+            while (el.hasChildNodes()) {
+                el.removeChild(el.lastChild as Node);
+            }
+        }
     }
-  }, [event]);
 
-  const MapLedgerDetailFn = async (fileOwnerNo: any) => {
-    try {
-      await dispatch(axiosGetLedgerDetail(fileOwnerNo));
-      isOpen(true);
-    } catch (e) {
-      Swal.fire({
-        title: '에러',
-        text: '관리자에게 문의 하세요',
-        icon: 'error',
-        confirmButtonText: '확인',
-        timer: 1000,
-      });
-
-      console.log(e);
+    // 지도 위에 표시되고 있는 마커를 모두 제거합니다
+    function removeMarker() {
+        markers.forEach(marker => marker.setMap(null));
+        markers = [];
     }
-  };
 
-  const ChangeEvent = () => {
-    if (changeEvent) {
-      setChangeEvent(false);
-    } else {
-      setChangeEvent(true);
-    }
-  };
+    const MapLedgerDetailFn = async (fileOwnerNo: number) => {
+        try {
+            await dispatch(axiosGetLedgerDetail(fileOwnerNo));
+            isOpen(true);
+        } catch (e) {
+            await Swal.fire({
+                title: '에러',
+                text: '관리자에게 문의 하세요',
+                icon: 'error',
+                confirmButtonText: '확인',
+                timer: 1000,
+            });
+            console.log(e);
+        }
+    };
 
-  const isOpen = (value: any) => {
-    setOpen(value);
-  };
+    const ChangeEvent = () => {
+        if (changeEvent) {
+            setChangeEvent(false);
+        } else {
+            setChangeEvent(true);
+        }
+    };
 
-  return (
-    <>
-      <div className='map_wrap'>
-        <div
-          id='MapLocation'
-          style={{ width: '100%', height: '100vh', position: 'relative', overflow: 'hidden' }}
-        ></div>
+    const isOpen = (value: any) => {
+        setOpen(value);
+    };
 
-        <div id='menu_wrap' className='bg_white'>
-          <hr />
-          <ul id='placesList'></ul>
-          <div id='pagination'></div>
-        </div>
-      </div>
+    return (
+        <>
+            <div className='map_wrap'>
+                <div
+                    id='MapLocation'
+                    style={{width: '100%', height: '100vh', position: 'relative', overflow: 'hidden'}}
+                ></div>
 
-      {ledger && <LedgerDetail categoryList={categoryList} ledger={ledger} isOpen={isOpen} open={open} />}
-    </>
-  );
+                <div id='menu_wrap' className='bg_white'>
+                    <hr/>
+                    <ul id='placesList'></ul>
+                    <div id='pagination'></div>
+                </div>
+            </div>
+
+            {ledger && <LedgerDetail categoryList={categoryList} ledger={ledger} isOpen={isOpen} open={open}/>}
+        </>
+    );
 }
--- src/main/front/src/MapComponont/StateMapLocation.tsx
+++ src/main/front/src/MapComponont/StateMapLocation.tsx
@@ -5,6 +5,7 @@ import { axiosGetLedgerDetail } from "../app/slices/ledgerSilce";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import Swal from "sweetalert2";
 import { RootState } from "../app/store";
+import {resolve} from "chart.js/helpers";
 
 
 // @ts-ignore
@@ -16,35 +17,47 @@ export default function StateMapLocation() {
   const [infowindow, setInfowindow] = useState<kakao.maps.InfoWindow | null>(null);
   const [map, setMap] = useState<kakao.maps.Map | undefined>();
   const [open, setOpen] = useState(false);
-  var markers: kakao.maps.Marker[] = [];
+  let markers: kakao.maps.Marker[] = [];
 
   useEffect(() => {
-    if (kakao && kakao.maps) {
-      const mapContainer = document.getElementById('MapLocation2'); // 지도를 표시할 div
-      const mapOption = {
-        center: new kakao.maps.LatLng(37.5665, 126.9780), // 지도의 중심 좌표 (서울)
-        level: 3, // 지도의 확대 레벨
-      };
+    const mapContainer = document.getElementById('MapLocation2'); // 지도를 표시할 div
+    const mapOption = {
+      center: new kakao.maps.LatLng(37.5665, 126.9780), // 지도의 중심 좌표 (서울)
+      level: 1, // 지도의 확대 레벨
+    };
 
-      if (mapContainer) {
-        const newMap = new kakao.maps.Map(mapContainer, mapOption);
-        setMap(newMap);
-        const newInfowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-        setInfowindow(newInfowindow);
+
+    if (kakao && kakao.maps && mapContainer) {
+      const newMap = new kakao.maps.Map(mapContainer, mapOption);
+      const newInfowindow = new kakao.maps.InfoWindow({zIndex: 1});
+
+      setMap(newMap);
+      setInfowindow(newInfowindow);
+  }
+  }, [])
+
+  useEffect(() => {
+    const triggerClick = async () => {
+      if (map && infowindow) {
+
+        setTimeout(async () => {
+          await displayPlaces(ledgerSeqNumbers);
+          $('.markerbg.marker_1').click();
+        }, 500);
+
       }
     }
-    setTimeout(() =>{
-      displayPlaces(ledgerSeqNumbers); // 실제 데이터를 여기에 넣으세요.
-    }, 1000)
-  }, [ledgerSeqNumbers]);
+    triggerClick().then(r => {return;});
+  }, [map, infowindow, ledgerSeqNumbers])
 
+  function displayPlaces(places: LedgerType[]) :  Promise<void> {
+    return new Promise( (resolve) => {
 
-  function displayPlaces(places: LedgerType[]) {
     const listEl = document.getElementById('placesList');
     const menuEl = document.getElementById('menu_wrap');
     const fragment = document.createDocumentFragment();
     const bounds = new kakao.maps.LatLngBounds();
- 
+
     removeAllChildNodes(listEl);
     removeMarker();
 
@@ -53,33 +66,33 @@ export default function StateMapLocation() {
       const marker = addMarker(placePosition, i, place.comment);
       const itemEl = getListItem(i, place); // 검색 결과 항목 Element를 생성합니다
 
-        bounds.extend(placePosition);
+      bounds.extend(placePosition);
 
-        kakao.maps.event.addListener(marker, 'mouseover', () => {
-          displayInfowindow(marker, place.location.address);
-        });
+      kakao.maps.event.addListener(marker, 'mouseover', () => {
+        displayInfowindow(marker, place.location.address);
+      });
 
-        kakao.maps.event.addListener(marker, 'click', () => {
-          MapLedgerDetailFn(place.fileOwnerNo);
-        });
+      kakao.maps.event.addListener(marker, 'click', () => {
+        MapLedgerDetailFn(place.fileOwnerNo);
+      });
 
-        kakao.maps.event.addListener(marker, 'mouseout', () => {
-          if(infowindow){
-            infowindow.close();
-          }
-        });
+      kakao.maps.event.addListener(marker, 'mouseout', () => {
+        if (infowindow) {
+          infowindow.close();
+        }
+      });
 
-        itemEl.onclick = () => {
-          displayInfowindowMoveLocation(marker, place.location.address);
-        };
+      itemEl.onclick = () => {
+        displayInfowindowMoveLocation(marker, place.location.address);
+      };
 
-        itemEl.onmouseout = () => {
-          if(infowindow){
-            infowindow.close();
-          }
-        };
+      itemEl.onmouseout = () => {
+        if (infowindow) {
+          infowindow.close();
+        }
+      };
 
-        fragment.appendChild(itemEl);
+      fragment.appendChild(itemEl);
     });
 
     if (listEl && menuEl) {
@@ -90,12 +103,15 @@ export default function StateMapLocation() {
     if (map) {
       map.setBounds(bounds);
     }
+
+     resolve(); // 프라미스를 완료합니다.
+    })
   }
 
-  // 검색결과 항목을 Element로 반환하는 함수입니다
-  function getListItem(index: number, place: LedgerType) {
-    const el = document.createElement('li');
-    const itemStr = `
+    // 검색결과 항목을 Element로 반환하는 함수입니다
+    function getListItem(index: number, place: LedgerType) {
+      const el = document.createElement('li');
+      const itemStr = `
       <span class="markerbg marker_${index + 1}"></span>
       <div style="display: flex;">
         <div class="info">
@@ -109,102 +125,102 @@ export default function StateMapLocation() {
         </div>
       </div>
     `;
-    el.innerHTML = itemStr;
-    el.className = 'item';
-    return el;
-  }
-
-  function addMarker(position: kakao.maps.LatLng, idx: number, title: string) {
-    const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png'; // 마커 이미지 url
-    const imageSize = new kakao.maps.Size(36, 37); // 마커 이미지의 크기
-    const imgOptions = {
-      spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-      spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-      offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-    };
-    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions);
-
-    const marker = new kakao.maps.Marker({
-      position: position,
-      image: markerImage,
-      map: map,
-    });
-
-    markers.push(marker); // 배열에 생성된 마커를 추가합니다
-    return marker;
-  }
-
-  // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
-  // 인포윈도우에 장소명을 표시합니다
-  function displayInfowindow(marker: kakao.maps.Marker, title: string) {
-    alert(33);
-    const content = `<div style="padding:5px;z-index:1;">${title}</div>`;
-    if (infowindow && map) {
-      infowindow.setContent(content);
-      infowindow.open(map, marker);
+      el.innerHTML = itemStr;
+      el.className = 'item';
+      return el;
     }
-  }
 
-  function displayInfowindowMoveLocation(marker: kakao.maps.Marker, title: string) {
-    alert(marker);
-    console.log(marker);
-    const content = `<div style="padding:5px;z-index:1;">${title}</div>`;
-    if (infowindow && map) {
-      infowindow.setContent(content);
-      infowindow.open(map, marker);
-      map.setCenter(marker.getPosition());
-      map.setLevel(4);
+    function addMarker(position: kakao.maps.LatLng, idx: number, title: string) {
+      const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png'; // 마커 이미지 url
+      const imageSize = new kakao.maps.Size(36, 37); // 마커 이미지의 크기
+      const imgOptions = {
+        spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+        spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+        offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+      };
+      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions);
+
+      const marker = new kakao.maps.Marker({
+        position,
+        image: markerImage,
+        map: map,
+      });
+
+      markers.push(marker); // 배열에 생성된 마커를 추가합니다
+      return marker;
     }
-  }
 
-  function removeAllChildNodes(el: HTMLElement | null) {
-    if (el) {
-      while (el.hasChildNodes()) {
-        el.removeChild(el.lastChild as Node);
+    // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
+    // 인포윈도우에 장소명을 표시합니다
+    function displayInfowindow(marker: kakao.maps.Marker, title: string) {
+      const content = `<div style="padding:5px;z-index:1;">${title}</div>`;
+      if (infowindow && map) {
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
       }
     }
-  }
 
-  // 지도 위에 표시되고 있는 마커를 모두 제거합니다
-  function removeMarker() {
-    markers.forEach(marker => marker.setMap(null));
-    markers = [];
-  }
+    function displayInfowindowMoveLocation(marker: kakao.maps.Marker, title: string) {
+      const content = `<div style="padding:5px;z-index:1;">${title}</div>`;
 
-  const MapLedgerDetailFn = async (fileOwnerNo: number) => {
-    try {
-      await dispatch(axiosGetLedgerDetail(fileOwnerNo));
-      isOpen(true);
-    } catch (e) {
-      Swal.fire({
-        title: '에러',
-        text: '관리자에게 문의 하세요',
-        icon: 'error',
-        confirmButtonText: '확인',
-        timer: 1000,
-      });
-      console.log(e);
+      if (infowindow && map) {
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
+        map.setCenter(marker.getPosition());
+        map.setLevel(4);
+      }
     }
-  };
 
-  const isOpen = (value: boolean) => {
-    setOpen(value);
-  };
+    function removeAllChildNodes(el: HTMLElement | null) {
+      if (el) {
+        while (el.hasChildNodes()) {
+          el.removeChild(el.lastChild as Node);
+        }
+      }
+    }
 
-  return (
-      <>
-        <div className='map_wrap'>
-          <div
-              id='MapLocation2'
-              style={{ width: '100%', height: '100vh', position: 'relative', overflow: 'hidden' }}
-          ></div>
+    // 지도 위에 표시되고 있는 마커를 모두 제거합니다
+    function removeMarker() {
+      markers.forEach(marker => marker.setMap(null));
+      markers = [];
+    }
 
-          <div id='menu_wrap' className='bg_white'>
-            <hr />
-            <ul id='placesList'></ul>
-            <div id='pagination'></div>
+    const MapLedgerDetailFn = async (fileOwnerNo: number) => {
+      try {
+        await dispatch(axiosGetLedgerDetail(fileOwnerNo));
+        isOpen(true);
+      } catch (e) {
+        await Swal.fire({
+          title: '에러',
+          text: '관리자에게 문의 하세요',
+          icon: 'error',
+          confirmButtonText: '확인',
+          timer: 1000,
+        });
+        console.log(e);
+      }
+    };
+
+    const isOpen = (value: boolean) => {
+      setOpen(value);
+    };
+
+
+
+    return (
+        <>
+          <div className='map_wrap'>
+            <div
+                id='MapLocation2'
+                style={{width: '100%', height: '100vh', position: 'relative', overflow: 'hidden'}}
+            ></div>
+
+            <div id='menu_wrap' className='bg_white'>
+              <hr/>
+              <ul id='placesList'></ul>
+              <div id='pagination'></div>
+            </div>
           </div>
-        </div>
-      </>
-  );
-}
+        </>
+    )
+  }
